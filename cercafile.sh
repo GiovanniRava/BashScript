07355451b1f816e950b4345588699ba3f145073e
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2024-12-14 12:09:36 +0100

    modified cercafile.sh

--- cercafile.sh
+++ cercafile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for car in {c..g}; do
-	for file in /usr/include/?${car}* ; do 
-		if [[ -e${name} && ( ${#name} -lt 18 || ${#name} -gt 23 ) ]] ; then 
+	for name in /usr/include/?${car}* ; do 
+		if [[ -e ${name} && ( ${#name} -lt 18 || ${#name} -gt 23 ) ]] ; then 
 			echo ${name}
 		fi
 	done
